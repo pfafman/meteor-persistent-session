f609fc15600d4a4a14fcc3cb1a285fd0a04951fe
Timothy Pfafman <tim@pfafman.com>
2021-03-03 07:39:04 -0700

    remove version

--- package.js
+++ package.js
@@ -1,12 +1,11 @@
 Package.describe({
   name: "pfafman:persistent-session",
-  version: "0.4.6",
+  version: "0.4.7",
   summary: "Persistently store Session data on the client",
   git: "https://github.com/pfafman/meteor-persistent-session"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('0.9.1'),
   api.use(['jquery', 'amplify', 'tracker', 'reactive-dict', 'session', 'underscore', 'ejson']);
   // If `accounts-base` is loaded, we have to make sure that this package is
   // loaded after `accounts-base` is, so we specify `weak: true` here
